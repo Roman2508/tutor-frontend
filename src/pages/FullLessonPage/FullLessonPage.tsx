529c6803130a4d0cc57a5a0c9d208b5c7ffcad2e
Roman Ptashnyk <2508romaa@gmail.com>
2024-02-20 14:43:47 +0200

    update lesson page file component

--- src/pages/FullLessonPage/FullLessonPage.tsx
+++ src/pages/FullLessonPage/FullLessonPage.tsx
@@ -1,26 +1,28 @@
-import React from "react"
-import cn from "classnames"
-import { Card } from "primereact/card"
-import { Ripple } from "primereact/ripple"
-import { Divider } from "primereact/divider"
-import { FaRegFile as FileIcon } from "react-icons/fa"
-import { FaRegFilePdf as PdfFileIcon } from "react-icons/fa6"
-import { FaRegFileWord as WordFileIcon } from "react-icons/fa"
-import { FaRegFileImage as ImageFileIcon } from "react-icons/fa"
-import { FaRegFilePowerpoint as PowerpointFileIcon } from "react-icons/fa6"
+import React from 'react'
+import cn from 'classnames'
+import { Card } from 'primereact/card'
+import { Ripple } from 'primereact/ripple'
+import { Divider } from 'primereact/divider'
+import { MdOutlineDownload } from 'react-icons/md'
+import { FaRegFile as FileIcon } from 'react-icons/fa'
+import { MdOutlineDeleteOutline } from 'react-icons/md'
+import { FaRegFilePdf as PdfFileIcon } from 'react-icons/fa6'
+import { FaRegFileWord as WordFileIcon } from 'react-icons/fa'
+import { FaRegFileImage as ImageFileIcon } from 'react-icons/fa'
+import { FaRegFilePowerpoint as PowerpointFileIcon } from 'react-icons/fa6'
 
-import styles from "./FullLessonPage.module.scss"
-import Avatar from "../../components/ui/Avatar/Avatar"
-import UploadFile from "../../components/LessonsPage/UploadFile"
-import { Button } from "primereact/button"
+import styles from './FullLessonPage.module.scss'
+import Avatar from '../../components/ui/Avatar/Avatar'
+import UploadFile from '../../components/LessonsPage/UploadFile'
+import { Button } from 'primereact/button'
 
 const FullLessonPage = () => {
   const files = [
-    { id: 1, name: "File name", icon: <FileIcon size={50} /> },
-    { id: 1, name: "File name File name File name ", icon: <PdfFileIcon size={50} /> },
-    { id: 1, name: "File name File name ", icon: <WordFileIcon size={50} /> },
-    { id: 1, name: "File name", icon: <PowerpointFileIcon size={50} /> },
-    { id: 1, name: "File name File name ", icon: <ImageFileIcon size={50} /> },
+    { id: 1, name: 'File name', icon: <FileIcon size={50} /> },
+    { id: 1, name: 'File name File name File name ', icon: <PdfFileIcon size={50} /> },
+    { id: 1, name: 'File name File name ', icon: <WordFileIcon size={50} /> },
+    { id: 1, name: 'File name', icon: <PowerpointFileIcon size={50} /> },
+    { id: 1, name: 'File name File name ', icon: <ImageFileIcon size={50} /> },
   ]
 
   return (
@@ -31,16 +33,16 @@ const FullLessonPage = () => {
             <Avatar size="large" shape="square" />
           </div>
 
-          <div className={cn(styles.col, styles["main-content"])}>
-            <h4 className={styles["user-name"]}>Boris Johnson</h4>
+          <div className={cn(styles.col, styles['main-content'])}>
+            <h4 className={styles['user-name']}>Boris Johnson</h4>
 
-            <p className={styles["lesson-name"]}>Англійська мова</p>
-            <p className={styles["lesson-theme"]}>Unit 4 Develop your Vocabulary pp. 100</p>
+            <p className={styles['lesson-name']}>Англійська мова</p>
+            <p className={styles['lesson-theme']}>Unit 4 Develop your Vocabulary pp. 100</p>
           </div>
 
           <div className={styles.col}>
             <div className={styles.s}>
-              <div className={styles["status-wrapper"]}>
+              <div className={styles['status-wrapper']}>
                 <div className={styles.badge}></div>
                 <p className={styles.status}>Заплановано</p>
               </div>
@@ -64,12 +66,27 @@ const FullLessonPage = () => {
             <div className={styles.file} key={el.id} title={`Завантажити: ${el.name}`}>
               {el.icon}
               <p>File name</p>
-              <Ripple
-                /* @ts-ignore */
-                pt={{
-                  root: { style: { background: "rgba(75, 175, 80, 0.3)" } },
-                }}
-              />
+              <div className={styles['file-controls']}>
+                <div className={styles['file-controls-icon']}>
+                  <MdOutlineDownload size={25} />
+                  <Ripple
+                    /* @ts-ignore */
+                    pt={{
+                      root: { style: { background: 'rgba(0, 0, 0, 0.2)' } },
+                    }}
+                  />
+                </div>
+
+                <div className={styles['file-controls-icon']}>
+                  <MdOutlineDeleteOutline size={25} />
+                  <Ripple
+                    /* @ts-ignore */
+                    pt={{
+                      root: { style: { background: 'rgba(0, 0, 0, 0.2)' } },
+                    }}
+                  />
+                </div>
+              </div>
             </div>
           ))}
         </div>
